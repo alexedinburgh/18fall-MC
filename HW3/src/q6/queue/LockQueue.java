bec7ca37a1ae2aab47813b36149c2cfc9b6ff4b2
SHEN_XIAOCHENG <845876310@qq.com>
2018-10-04 18:04:18 -0500

    lockQueue

--- HW3/src/q6/queue/LockQueue.java
+++ HW3/src/q6/queue/LockQueue.java
@@ -1,20 +1,73 @@
 package q6.queue;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Implement Lock-based and Lock-Free unbounded queues of
+ * Integers. For the lock based implementation, use di  erent locks for enq and deq operations. For the
+ * variable count use AtomicInteger. For the lock-free implementation, use Michael and Scott's algorithm
+ * as explained in the class. The deq operation should return null if the queue is empty.
+ */
 public class LockQueue implements MyQueue {
 // you are free to add members
+    private Node head;
+    private Node tail;
+    private ReentrantLock qLock;
 
     public LockQueue() {
         // implement your constructor here
+        this.head = new Node(null);
+        this.tail = head;
+        this.qLock = new ReentrantLock();
     }
+
+    public int size() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Node node = head;
+        while(node.next != null) {
+            node = node.next;
+            counter.getAndIncrement();
+        }
+        return counter.get();
+    }
+
+    /**
+     * add new node into queue, keep FIFO
+     * @param value
+     * @return
+     */
 
     public boolean enq(Integer value) {
         // implement your enq method here
-        return false;
+        if (value == null) {
+            return false;
+        }
+        qLock.lock();
+        try {
+            Node newNode = new Node(value);
+            tail.next = newNode;
+            tail = newNode;
+            return true;
+        } finally {
+            qLock.unlock();
+        }
     }
 
     public Integer deq() {
+        Integer res;
+        qLock.lock();
+        try {
+            while (this.size() == 0);//wait for nodes
+            res = head.next.value;
+            head = head.next;
+        } finally {
+            qLock.unlock();
+        }
         // implement your deq method here
-        return null;
+        return res;
     }
 
     protected class Node {
